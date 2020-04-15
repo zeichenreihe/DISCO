edb8944809191c61f0b92811bae7a17aef56902a
Johannes Schmatz <johannes@kirsche.lan>
2020-04-16 00:23:17 +0200

    adding license to sed* files

--- sed_space_to_tab.sh
+++ sed_space_to_tab.sh
@@ -3,6 +3,7 @@ if [[ $# != 2 ]];
 then
 	echo "usage: $0 <in_file> <outfile>"
 	echo "replace all tabs with spaces"
+	echo "LICENSE GPLv3"
 else
 	cat $1 | sed -e "s/        /\t/g"  > $2
 fi
--- sed_tab_to_space.sh
+++ sed_tab_to_space.sh
@@ -3,6 +3,7 @@ if [[ $# != 2 ]];
 then
 	echo "usage: $0 <in_file> <outfile>"
 	echo "replace all tabs with spaces"
+	echo "LICENSE GPLv3"
 else
 	cat -T $1 | sed -e 's/\^\I/        /g'  > $2
 fi
