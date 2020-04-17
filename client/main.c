c7d048710cc15477c382cf1103f274dab9de122a
Johannes Schmatz <johannes@pixy>
2020-04-17 23:51:06 +0200

    mv description in client to onliner in exra func.

--- client/main.c
+++ client/main.c
@@ -19,63 +19,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv){
-        if( argc == 1 ){
-                printf("NAME\n");
-                printf("        %s - a client program for DISCO\n\n", argv[0]);
-                printf("SYNOPSYS\n");
-                printf("        %s file [-s]|-l|-n\n", argv[0]);
-                printf("        %s [--help]\n\n", argv[0]);
-                printf("DESCRIPTION");
-                printf("        %s is the client program for the big Arduino program, that\n", argv[0]);
-                printf("        plays music. It allows you to play music with or without\n");
-                printf("        leds or to play only leds.\n\n");
-                printf("        You always need a serial device as argument 1 and you might\n");
-                printf("        need root rights, so maybe type a 'sudo' in front of your command.\n\n");
-                printf("        Options:\n\n");
-                printf("                -s      plays on the Arduino the song with the leds.\n");
-                printf("                -l      shows only the leds.\n");
-                printf("                -n      plays the song without leds.\n\n");
-                printf("EXIT STATUS\n");
-                printf("        The exit status is 1 if the helptext was shown and it is 0 if there was\n");
-                printf("        no error.\n\n");
-                printf("AUTHOR\n");
-                printf("        Written by Johannes Schmatz.\n\n");
-                printf("COPYRIGHT\n");
-                printf("        Arduino client program for NWT\n");
-                printf("        Copyright (C) 2020  Johannes Schmatz\n\n");
-                printf("        This program is free software: you can redistribute it and/or modify\n");
-                printf("        it under the terms of the GNU General Public License as published by\n");
-                printf("        the Free Software Foundation, either version 3 of the License, or\n");
-                printf("        (at your option) any later version.\n\n");
-                printf("        This program is distributed in the hope that it will be useful,\n");
-                printf("        but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
-                printf("        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n");
-                printf("        GNU General Public License for more details.\n\n");
-                printf("        You should have received a copy of the GNU General Public License\n");
-                printf("        along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n");
-                printf("EXAMPLES\n");
-                printf("        %s /dev/ttyUSB0       plays music.\n", argv[0]);
-                printf("        %s /dev/ttyUSB0 -l    plays the leds.\n", argv[0]);
-                printf("        %s /dev/ttyUSB0 -n    play music without leds.\n", argv[0]);
-                printf("        %s                    print this helptext.\n", argv[0]);
-                return 1;
-        }
+int helptext(char *name){
+	printf("NAME\n\t%s - a client programm for DISCO\n\nSYNOPSYS\n\t%s file [-s]|-l|-n\n\t%s [--help]\n\nDESCRIPTION\n\t%s is the client programm for the big Arduino program, that\n\tplays music. It allows you to play music with or without\n\tleds or to play only leds.\n\n\tYou always need a serial device as argument 1 and you might\n\tneed root rights, so maybe type a 'sudo' in front of your command.\n\n\tOptions:\n\n\t\t-s\tplays on the Arduino the song with the leds.\n\t\t-l\t shows only the leds.\n\t\t-n\tplays the song without leds.\n\t\t--help\tprints this help\n\nEXIT STATUS\n\tThe exit status is 1 if the helptext was shown and it is 0 if there was\n\tno error.\n\nAUTHOR\n\tWritten by Johannes Schmatz.\n\nCOPYRIGHT\n\tYou should have received a copy of the GNU General Public License\n\talong with this program.  If not, see <http://www.gnu.org/licenses/>.\n\tsee LICENSE\n\nEXAMPLES\n\t%s /dev/ttyUSB0\tplays music.\n\t%s /dev/ttyUSB0 -l\tplays the leds.\n\t%s /dev/ttyUSB0 -n\tplay music without leds.\n\t%s --help\t\tprint this helptext.\n", name, name, name, name, name, name, name, name);
+	return 1;
+}
 
+int main(int argc, char **argv){
+	if( argc == 1 ){
+		return helptext(argv[0]);
+	}
+	if( argv[1] == "--help" ){
+		return helptext(argv[0]);
+	}
 
 	FILE *fileptr;
-        fileptr = fopen(argv[1], "w");
+	fileptr = fopen(argv[1], "w");
 
-        if(argv[2] == "-l"){
-                fprintf(fileptr, "1");
-        }else if(argv[2] == "-n"){
-                fprintf(fileptr, "2");
-        }else{
-                fprintf(fileptr, "0");
-        }
+	if(argv[2] == "-l"){
+		fprintf(fileptr, "1");
+	}else if(argv[2] == "-n"){
+		fprintf(fileptr, "2");
+	}else{
+		fprintf(fileptr, "0");
+	}
 
 	system("sleep 3");
 	fclose(fileptr);
-	
-        return 0;
+
+	return 0;
 }
+
+// syntax
+// vim: ts=8 sts=8 sw=8 noet si syntax=c
