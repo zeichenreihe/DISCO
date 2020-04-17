cf044c01ebf00f8766538efc7e5b2b9e7b101eb1
Johannes Schmatz <johannes@pixy>
2020-04-17 22:55:10 +0200

    adding GPLv3 liense header in sed_*_to_*.sh files

--- sed_space_to_tab.sh
+++ sed_space_to_tab.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+#    sed_space_to_tab.sh - shell script to convert spaces to tabs
+#    Copyright (C) 2020  Johannes Schmatz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [[ $# != 2 ]];
 then
 	echo "usage: $0 <in_file> <outfile>"
-	echo "replace all tabs with spaces"
+	echo "replace all spaces with tabs"
 	echo "LICENSE GPLv3"
 else
 	cat $1 | sed -e "s/        /\t/g"  > $2
 fi
+
+# syntax
+# vim: ts=8 sts=8 sw=8 noet si syntax=bash
--- sed_tab_to_space.sh
+++ sed_tab_to_space.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#    sed_tab_to_space.sh - shell script to convert tabs to spaces
+#    Copyright (C) 2020  Johannes Schmatz
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [[ $# != 2 ]];
 then
 	echo "usage: $0 <in_file> <outfile>"
@@ -7,3 +23,6 @@ then
 else
 	cat -T $1 | sed -e 's/\^\I/        /g'  > $2
 fi
+
+# syntax
+# vim: ts=8 sts=8 sw=8 noet si syntax=bash
